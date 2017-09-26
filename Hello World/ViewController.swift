9f74d3afbd93847f5fc36911938e141a9dc7f8a7
User <user@Users-MacBook-Pro.local>
2017-09-26 14:18:39 -0700

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by User on 9/26/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello World")
+    }
+}
+
